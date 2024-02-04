96e2b4c149af40f462e5fdf532809f22442c5e4c
Nitish Yadav <nitishyadav5292@gmail.com>
2024-02-04 15:03:57 +0530

    Home Page functionality

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -8,7 +8,7 @@ export default function ListingItem({listing}) {
             overflow-hidden rounded-lg w-full sm:w-[300px]'>
         <Link to={`/listing/${listing._id}`}>
             <img src={listing.imageUrls[0]} alt='listing cover'
-            className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105
+            className='h-[300px] sm:h-[250px] w-full object-cover hover:scale-105
             transition-scale duration-300'></img>
             <div className="p-3 flex flex-col gap-2 w-full">
                 <p className='truncate text-lg font-semibold text-slate-700'>{listing.name}</p>
